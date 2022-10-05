fcdf12302cf54904fbda0ae4a265b4b924e71399
Naveen <navaneethk002@gmail.com>
2022-10-04 21:20:14 -0500

    front-end

--- app/copyAi_api.py
+++ app/copyAi_api.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from call_gpt3 import generate_branding, generate_keywords
 from mangum import Mangum
-
-MAX_PROMPT_LEN = 32
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 handler = Mangum(app)
+MAX_INPUT_LENGTH = 32
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/generate_branding")
 async def generate_branding_api(prompt: str):
@@ -27,6 +36,6 @@ async def generate_branding_and_keywords_api(prompt: str):
     return {"branding": branding, "keywords": keywords}
 
 def validate_input(prompt):
-    if len(prompt) >= MAX_PROMPT_LEN:
-        raise HTTPException(status_code = 400, detail = f"Length of input is too long, must be less than {MAX_PROMPT_LEN} characters")
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(status_code = 400, detail = f"Length of input is too long, must be less than {MAX_INPUT_LENGTH} characters")
 
--- copykitt-site/pages/index.tsx
+++ copykitt-site/pages/index.tsx
@@ -1,20 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import CopyKitt from "../components/CopyKitt";
+import CopyKitt from "../components/copykitt";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>CopyKitt Tutorial | AI Generated Marketing</title>
         <meta
           name="description"
-          content="Generated branding snippets and keywords for your product"
+          content="Generate branding snippets for your product."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <CopyKitt />
     </div>
   );
